cd7cd67dbaa7191d72d6711e50b8a4d8f9203c77
pcanizares <p_canizares@hotmail.com>
2020-02-02 17:50:33 +0100

    Clase active para el menú

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,6 +7,17 @@
     
     @yield('title')
 
+    <style>
+      nav li a{
+        text-decoration: none;
+        color: purple;
+      }
+
+      nav li.active a {
+        color: red;
+      }
+    </style>
+
   </head>
 
   <body>
@@ -16,17 +27,18 @@
     </header>
 
     <nav>
+      {{ strpos(Route::currentRouteName(), 'bookdd') }}
       <ul>
-        <li>
+        <li @if(Route::currentRouteName() == 'index') class='active' @endif>
           <a href="{{ route('index') }}">Inicio</a>
         </li>
-        <li>
+        <li @if(Route::currentRouteName() == 'mvc') class='active' @endif>
           <a href="{{ route('mvc') }}">MVC</a>
         </li>
-        <li>
+        <li @if(Route::currentRouteName() == 'laravel') class='active' @endif>
           <a href="{{ route('laravel') }}">Laravel</a>
         </li>
-        <li>
+        <li @if(strpos(Route::currentRouteName(), 'book') === 0 || strpos(Route::currentRouteName(), 'book') >= 1) class='active' @endif>
           <a href="{{ route('books-list') }}">Books</a>
         </li>
       </ul>
