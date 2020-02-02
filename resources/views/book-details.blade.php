9b43029ed59a99ab19673641224603587161104f
pcanizares <p_canizares@hotmail.com>
2020-02-02 21:13:11 +0100

    CSS: Detalle de un libro

--- resources/views/book-details.blade.php
+++ resources/views/book-details.blade.php
@@ -2,18 +2,65 @@
 
 @section('title')
   <title>Detalle de un libro</title>
+
+  <style> 
+    .jumbotron {
+      background-image: url('../../images/libros2.jpg');
+      background-repeat: no-repeat;
+      background-blend-mode: soft-light !important;
+      background-size: cover;
+    }
+  </style>
+
 @endsection
 
 
 @section('content')
 
+  <div class="jumbotron">
+    <h1 class="display-4">Libros</h1>
+    <p class="lead">Detalle del libro</p>
+    <hr class="my-4">
+    <p>¿Te gusta leer? ¡Quizás este libro pueda gustarte!.</p>
+    @include('includes/back-to-books')
+  </div>
+
+
+  <div class='container'>
+
+    @if($book)
+
+      <h1 class='text-center'>{{ $book->title }}</h1>
+
+      <div class='text-center mt-4'>
+        <p><b>ISBN:</b> {{ $book->isbn }}</p>
+
+        <p><b>Autor:</b> {{ $book->author }}</p>
+
+        <p><b>Número de páginas:</b> {{ $book->pages }}</p>
+      </div>
+
+      <div class='text-justify mt-4'>
+        <h3>Sinopsis:</h3>
+
+        <p>{{ $book->sinopsis }}</p>
+      </div>
+
+    @else
+
+      <h3 class='text-center'>El libro no existe</h3>
+
+    @endif
+
+  </div>
+
   {{-- <div>
     <a href="{{ route('books-list') }}">Volver al listado de libros</a>
   </div> --}}
 
-  @include('includes/back-to-books')
+  {{-- @include('includes/back-to-books') --}}
 
-  <div>
+  {{-- <div>
 
     @if($book)
 
@@ -35,7 +82,7 @@
 
     @endif
 
-  </div>
+  </div> --}}
 
 @endsection
 
