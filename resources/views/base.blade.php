509b9c3c080af79e464db852d88b39803c390e66
pcanizares <p_canizares@hotmail.com>
2020-02-02 19:42:44 +0100

    CSS: Pagina MVC

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,48 +4,82 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">    
+
     @yield('title')
 
     <style>
-      nav li a{
-        text-decoration: none;
-        color: purple;
-      }
-
-      nav li.active a {
-        color: red;
+      #content {
+        margin-top:56px;
       }
     </style>
 
   </head>
 
   <body>
-
+{{-- 
     <header>
       CABECERA
-    </header>
+    </header> --}}
 
-    <nav>
-      {{ strpos(Route::currentRouteName(), 'bookdd') }}
-      <ul>
-        <li @if(Route::currentRouteName() == 'index') class='active' @endif>
-          <a href="{{ route('index') }}">Inicio</a>
-        </li>
-        <li @if(Route::currentRouteName() == 'mvc') class='active' @endif>
-          <a href="{{ route('mvc') }}">MVC</a>
-        </li>
-        <li @if(Route::currentRouteName() == 'laravel') class='active' @endif>
-          <a href="{{ route('laravel') }}">Laravel</a>
-        </li>
-        <li @if(strpos(Route::currentRouteName(), 'book') === 0 || strpos(Route::currentRouteName(), 'book') >= 1) class='active' @endif>
-          <a href="{{ route('books-list') }}">Books</a>
-        </li>
-      </ul>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">My Books</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li @if(Route::currentRouteName() == 'index') class='nav-item active' @else class='nav-item' @endif >
+            <a class="nav-link" href="{{ route('index') }}">Inicio</a>
+          </li>
+          <li @if(Route::currentRouteName() == 'mvc') class='nav-item active' @else class='nav-item' @endif>
+            <a class="nav-link" href="{{ route('mvc') }}">MVC</a>
+          </li>
+          <li @if(Route::currentRouteName() == 'laravel') class='nav-item active' @else class='nav-item' @endif>
+            <a class="nav-link" href="{{ route('laravel') }}">Laravel</a>
+          </li>
+          <li @if(strpos(Route::currentRouteName(), 'book') === 0 || strpos(Route::currentRouteName(), 'book') >= 1) class='active' @else class='nav-item' @endif>
+            <a class="nav-link" href="{{ route('books-list') }}">Libros</a>
+          </li>
+
+        </ul>
+      </div>
     </nav>
 
-    @yield('content')
+    <div id='content'>
 
+      @yield('content')
+
+    </div>
+
+    <footer class='container-fluid mt-5'>
+      <div class='border-top pt-4 pb-4'>
+        <div class='row'>
+          <div class='col'>My Books &copy;{{ date('Y') }}</div>
+          <div class='col text-right'>
+            <a href='#' class='ml-3'>
+              <i class='fa fa-facebook'></i>
+            </a>
+            <a href='#' class='ml-3'>
+              <i class='fa fa-twitter'></i>
+            </a>
+            <a href='#' class='ml-3'>
+              <i class='fa fa-instagram'></i>
+            </a>
+            
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    
   </body>
 </html>
 
--- resources/views/mvc.blade.php
+++ resources/views/mvc.blade.php
@@ -2,11 +2,73 @@
 
 @section('title')
   <title>MVL</title>
+
+  <style> 
+    .jumbotron {
+      background-image: url('images/mvc-modelo-vista-controlador-portada.png');
+      border-radius: 0px;
+      background-attachment: fixed;
+      background-repeat: repeat-x;
+      background-blend-mode: color-burn;
+    }
+  </style>
+
 @endsection
 
 
 @section('content')
 
-  PÁGINA PARA EXPLICAR QUE ES EL MVC
+  <div class="jumbotron">
+    <h1 class="display-4">MVC</h1>
+    <p class="lead">Modelo-Vista-Controlador</p>
+    <hr class="my-4">
+    <p>¿Qué es y para qué sirve?</p>
+  </div>
+
+  <div class='container'>
+    
+    <h1 class='text-center'>MVC (Modelo-Vista-Controlador): ¿qué es y para qué sirve?</h1>
+
+    <div class='text-justify mt-4'>
+
+      <p>
+      Tal vez has escuchado hablar acerca de este modelo, ya que es una excelente practica para definir la arquitectura de muchos sistemas y a los programadores nos permite estructurar sistemas robustos de forma clara y eficiente, sobre todo pensando en que los sistemas sean escalables y requerirán mantenimiento. Si aún no conoces este modelo, aquí te explico en qué consiste.<br />
+      </p>
+
+      <p>
+        El <b>MVC </b> es un <strong>patrón de diseño arquitectónico de </strong><i>software</i></strong>, que sirve para 
+        <strong>clasificar la información</strong>, <strong>la lógica del sistema </strong>y<strong> la interfaz</strong> 
+        que se le presenta al usuario. En este tipo de arquitectura existe un sistema central o controlador que gestiona las entradas y
+        la salida del sistema, uno o varios modelos que se encargan de buscar los datos e información necesaria y una interfaz que muestra los resultados al usuario final. 
+        Es muy usado en el desarrollo web porque al tener que interactuar varios lenguajes para crear un sitio es muy fácil generar confusión entre cada componente si 
+        estos no son separados de la forma adecuada. Este patrón permite modificar cada uno de sus componentes si necesidad de afectar a los demás.
+      </p>
+
+      <h2>Componentes</h2>
+
+      <div class='text-center'>
+        <img src='images/mvc-modelo-vista-controlador.png' class="img-fluid">
+      </div>
+
+      <p>
+        <b>Modelo:</b> este componente se encarga de manipular, gestionar y actualizar los datos. Si se utiliza una base de datos aquí es donde se realizan las consultas, búsquedas, filtros y actualizaciones.
+      </p>
+      
+      <p>
+        <b>Vista:</b> este componente se encarga de mostrarle al usuario final las pantallas, ventanas, páginas y formularios; el resultado de una solicitud. Desde la perspectiva del programador este componente es el que se encarga del frontend; la programación de la interfaz de usuario si se trata de un aplicación de escritorio, o bien, la visualización de las páginas web (CSS, HTML, HTML5 y Javascript).
+      </p>
+      
+      <p>
+        <b>Controlador:</b> este componente se encarga de gestionar las instrucciones que se reciben, atenderlas y procesarlas. Por medio de él se comunican el modelo y la vista: solicitando los datos necesarios; manipulándolos para obtener los resultados; y entregándolos a la vista para que pueda mostrarlos.
+      </p>
+        
+      <p>
+        Este patrón es uno de los más usados, en la actualidad se puede encontrar tanto en pequeños como en grandes sistemas, en el mundo laboral es indispensable llevarlo a la practica. Si apenas te estás iniciando en el mundo de la programación, te recomiendo que adoptes este modelo lo más pronto posible, para que en un futuro con sistemas más complejos no tengas ningún inconveniente.
+      </p>
+
+    </div>
+
+  </div>
+
 
 @endsection
